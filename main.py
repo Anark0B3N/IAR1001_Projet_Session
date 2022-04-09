593c70d8f9a4515ac337498706646426f1d9be28
allaryt <poki114poki114@outlook.com>
2022-04-09 12:14:10 -0400

    tentative reconnaissance de droite et de cercles

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import math
 import cv2
 import glob
 import numpy as np
+from mnist import MNIST
 
 def main():
-    test_knn()
+
+
+
+    # or
+    # images, labels = mndata.load_testing()
+
+    Hough()
 
     # quit = False
     # while(not quit):
@@ -174,11 +181,66 @@ def test_knn():
     print(neighbourgs)
     print("image est un " + maxLabel.__str__())
 
-def findKNearestNeigbours(listOfKnownImg, k, imgToTest):
+
+def knn2():
+    mndata = MNIST('samples')
+    listOfImages = []
+
+    #60 000 images of 784 pixels (28x28)
+    #60 000 labels
+    images, labels = mndata.load_training()
+    images_test, labels_test = mndata.load_testing()
+
+    imgIdx = 0
+    for img in images:
+        listOfImages.append([labels[imgIdx], img])
+
+        imgIdx += 1
+
+    # imageFaitParTom = cv2.imread("test-nb-2.png", -1)
+    # imgray = cv2.cvtColor(imageFaitParTom, cv2.COLOR_BGR2GRAY)
+    # imgTest = np.array(imgray).flatten()
+
+
+    testAccuraty = 0
+    imgCount = 0
+    for imgToTest in images_test:
+        neighbourgs = findKNearestNeigbours(listOfImages, 77, imgToTest, 784)
+
+        countByNumber = {}
+        for node in neighbourgs:
+            if countByNumber.__contains__(node[0]):
+                countByNumber[node[0]] += 1
+            else:
+                countByNumber[node[0]] = 1
+
+        maxCount = 0
+        maxLabel = -1
+        for count in countByNumber:
+            if countByNumber[count] > maxCount:
+                maxCount = countByNumber[count]
+                maxLabel = count
+
+        print(neighbourgs)
+        print("image est un " + maxLabel.__str__())
+        print(imgCount.__str__() + " sur 200")
+
+        if imgCount > 2000:
+            break
+
+        if maxLabel == labels_test[imgCount]:
+            testAccuraty += 1
+
+        imgCount += 1
+
+    print("accuracy over " + imgCount.__str__() + " testing images:")
+    print((testAccuraty / imgCount) * 100)
+
+def findKNearestNeigbours(listOfKnownImg, k, imgToTest, imgSize = 400):
 
     distByNumber = []
     for img in listOfKnownImg:
-        dist = distBetweenImages(img[1], imgToTest)
+        dist = distBetweenImages(img[1], imgToTest, imgSize)
         distByNumber.append([img[0], dist])
 
     distByNumber.sort(key=sortSecond)
@@ -193,12 +255,96 @@ def findKNearestNeigbours(listOfKnownImg, k, imgToTest):
 def sortSecond(val):
     return val[1]
 
-def distBetweenImages(img1, img2):
+def distBetweenImages(img1, img2, imgSize = 400):
     dimensionDistancesSum = 0
-    for x in range(400):
+    for x in range(imgSize):
         dimensionDistancesSum += math.pow((img1[x] - img2[x]) , 2)
 
     return math.sqrt(dimensionDistancesSum)
+
+
+def Hough():
+    listOfImages = []
+
+    myimg = cv2.imread("digits.png", -1)
+    imageFaitParTom = cv2.imread("test-nb-2.png", -1)
+    imgray = cv2.cvtColor(imageFaitParTom, cv2.COLOR_BGR2GRAY)
+
+    imgTest = np.array(imgray).flatten()
+    cells = [np.hsplit(row, 100) for row in np.vsplit(myimg, 50)]
+
+    # Make it into a Numpy array: its size will be (50,100,20,20)
+    imgTable = np.array(cells)
+
+
+    for img in imgTable[44][0:4]:
+        strong_lines = []
+        hough_circles = []
+
+        img = (255 - img)
+        img = cv2.resize(img, (150, 150))
+
+        #circle...
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=20, minRadius=10, maxRadius=90)
+
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            # circle outline
+            radius = i[2]
+            hough_circles.append(HoughCircle(center, radius))
+
+
+        #lines...
+        edges = cv2.Canny(img, 100, 200)
+        lines = cv2.HoughLines(edges, 1, np.pi/180, 14)
+
+        for r_teta in lines:
+            rho, teta = r_teta[0]
+            strong = True
+            for houghLine in strong_lines:
+                if abs(houghLine.rho - rho) < 20 and abs(teta - houghLine.theta) < 6:
+                    strong = False
+                    break
+
+            if strong:
+                strong_lines.append(HoughStrong(rho, teta))
+
+
+
+        # draw lines and circles
+        for circle in hough_circles:
+            cv2.circle(img, circle.center, circle.r, (80, 0, 0), 2)
+
+        for line in strong_lines:
+            a = np.cos(line.theta)
+            b = np.sin(line.theta)
+            x0 = a * line.rho
+            y0 = b * line.rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+
+            cv2.line(img, (x1, y1), (x2, y2), (80, 0, 0), 1)
+
+
+
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+class HoughStrong:
+    def __init__(self, rho, theta):
+        self.rho = rho
+        self.theta = theta
+
+class HoughCircle:
+    def __init__(self, center, r):
+        self.r = r
+        self.center = center
 
 
 # Press the green button in the gutter to run the script.
